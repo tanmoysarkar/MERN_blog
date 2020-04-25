9184d62a21b5f5c7c396005672f11644ba344731
Tanmoy Sarkar <tnmsarkar96@gmail.com>
2020-04-25 18:05:31 +0530

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 const auth = require('../../middleware/auth');
 
 //User Model 
-const User = require('../../models/User');
+const User = require('../../models/user');
 
 //@route GET api/auth
 // @desc auth the user
